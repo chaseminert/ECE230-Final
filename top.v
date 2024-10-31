bdf985a6c733225a4c60a08fafc70abda4fa13c5
Chase Minert <chaseminert@u.boisestate.edu>
2024-10-31 16:57:25 -0600

    added all operations

--- top.v
+++ top.v
@@ -1,19 +1,47 @@
 module top (
 
-input [3:0] sw,  //operation select
-input [15:8] sw_input_data, //8 bit input data
+input [15:0] sw,  //operation select
 input btnC,   // button operation rising edge clock
-output reg [15:8] led_A,  
-output reg [7:0] led_B, 
+output [15:0] led,
 output reg [6:0] segs,
-output reg [3:0] an
-
+output reg [3:0] an,
 );
 
+wire [7:0] sw_input_data;
+assign sw_input_data = sw[15:8];
 
+wire[3:0] operation_select;
+assign operation_select = sw[3:0];
+
+wire[7:0] led_A;
+assign led[15:8] = led_A; 
+
+wire[7:0] led_B;
+assign led[7:0] = led_B; 
+
+reg [7:0] Y;
 reg [7:0] A, B;  //signed 8 bit register for A and B
-reg [7:0] Y;     //Output register for result
+reg [7:0] tmp;
+wire [7:0] adder_output;
+wire [7:0] sub_output;
+wire [7:0] twos_comp_output
 
+eight_bit_adder add(
+    .A(A),
+    .B(B),
+    .Sum(full_adder_output)
+);
+
+eight_bit_sub sub(
+    .A(A),
+    .B(B),
+    .Y(sub_output)
+);
+
+twos_complement comp(
+    .Input(A),
+    .Output(twos_comp_output)
+);
 
 
 always @(posedge btnC | posedge reset)begin
@@ -26,18 +54,41 @@ always @(posedge btnC | posedge reset)begin
     end else begin
         case(sw)
 
-        4'b0000: Y = //call full adder
-        4'b0001: Y = //call subtractor
-        4'b0010: Y = << 1; //shift left
-        4'b0011: Y = >> 1; //shift right
-
-
+        4'd0: Y = full_adder_output;
+        4'd1: Y = sub_output;
+        4'd2: Y = << 1; //shift left
+        4'd3: Y = >> 1; //shift right
+        4'd4: begin
+            if (A == B) begin
+                Y <= 0;
+            end else if (A > B) begin
+                Y <= 1;
+            end else begin
+                Y <= -1;
+            end
+        end
+        4'd5: Y <= A & B;
+        4'd6: Y <= A | B;
+        4'd7: Y <= A ^ B;
+        4'd8: Y <= A ~& B;
+        4'd9: Y <= A ~| B;
+        4'd10: Y <= A ~^ B;
+        4'd11: Y <= ~A;
+        4'd12: Y <= twos_comp_output;
+        4'd13: A <= Y;
+        4'd14: begin
+            B <= A;
+            A <= B; 
+        end
+        4'd15: A <= sw_input_data;
         endcase
 
 
 end
 
 end
+
+assign led[]
 
 
 
